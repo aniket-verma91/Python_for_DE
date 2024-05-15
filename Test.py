8cdc925a63e5c1c4fe57d670a6f48351cd684294
Aniket Verma <aniketverma1991@gmail.com>
2024-05-15 22:57:42 +0530

    Two files commiting

--- Test.py
+++ Test.py
@@ -1,2 +1,9 @@
+# Python3 program to add two numbers
+num1 = 15
+num2 = 12
 
-print("Hello, World!")
+# Adding two nos
+sum = num1 + num2
+
+# printing values
+print("Sum of", num1, "and", num2, "is", sum)
